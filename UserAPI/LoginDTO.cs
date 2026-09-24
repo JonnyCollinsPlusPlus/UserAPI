83b98fd069abe3fdc498eed7dcb96cd53ce64ef0
Jonathan Collins <jonny.collins111@gmail.com>
2026-09-24 20:18:33 +0100

    added required flags for email and password in loginDTO

--- UserAPI/LoginDTO.cs
+++ UserAPI/LoginDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace UserAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAPI.DTOs
 {
     public class LoginDTO
     {
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+        [Required]
         public string Password { get; set; } = "User";
     }
 }
